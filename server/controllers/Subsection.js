b299565c1c551a3b47ebc3cc187b93671fb80093
saumayyyy <109162520+saumayyyy@users.noreply.github.com>
2023-10-15 01:00:28 +0530

    Update Subsection.js

--- server/controllers/Subsection.js
+++ server/controllers/Subsection.js
@@ -1,6 +1,6 @@
 // Import necessary modules
 const Section = require("../models/Section");
-const SubSection = require("../models/Subsection");
+const SubSection = require("../models/SubSection");
 const { uploadImageToCloudinary } = require("../utils/imageUploader");
 
 // Create a new sub-section for a given section
