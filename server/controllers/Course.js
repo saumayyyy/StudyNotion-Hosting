a866741d76ff7acf7585c59ddcfeb1119698cfe1
saumayyyy <109162520+saumayyyy@users.noreply.github.com>
2023-10-15 00:54:17 +0530

    Update Course.js

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,7 +1,7 @@
 const Course = require("../models/Course")
 const Category = require("../models/Category")
 const Section = require("../models/Section")
-const SubSection = require("../models/Subsection")
+const SubSection = require("../models/SubSection")
 const User = require("../models/User")
 const { uploadImageToCloudinary } = require("../utils/imageUploader")
 const CourseProgress = require("../models/CourseProgress")
