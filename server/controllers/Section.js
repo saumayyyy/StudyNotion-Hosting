6bef538e96814aea53808c1a7b45d85c598c4814
saumayyyy <109162520+saumayyyy@users.noreply.github.com>
2023-10-15 01:00:12 +0530

    Update Section.js

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -1,6 +1,6 @@
 const Section = require("../models/Section")
 const Course = require("../models/Course")
-const SubSection = require("../models/Subsection")
+const SubSection = require("../models/SubSection")
 // CREATE a new section
 exports.createSection = async (req, res) => {
   try {
