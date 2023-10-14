a0d95e2c4082a751b083c947ba78d7ca382c063d
saumayyyy <109162520+saumayyyy@users.noreply.github.com>
2023-10-15 01:06:26 +0530

    Update Course.js

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -43,7 +43,7 @@ const {
   createRating,
   getAverageRating,
   getAllRatingReview,
-} = require("../controllers/RatingandReview")
+} = require("../controllers/RatingAndReview")
 const {
   updateCourseProgress,
   getProgressPercentage,
